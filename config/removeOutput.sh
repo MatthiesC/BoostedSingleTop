ca937daabcf965600867ab7d18fc674f374297b2
Christopher Matthies <Christopher.Matthies@desy.de>
2018-08-17 22:19:42 +0200

    updated removeOutput.sh

--- config/removeOutput.sh
+++ config/removeOutput.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-read -p "Do you really want do delete workdirs and output data? (y/n) " answer
+read -p "Do you really want do delete ANALYSIS workdirs and output data? (Type e/m for electron/muon channel) " answer
 
-if [ "$answer" == "n" ]
+if [ "$answer" == "m" ]
     then
-    exit 1
-elif [ "$answer" == "y" ]
+    rm -r /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Muon/workdir_analysis_muo/
+    rm /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Muon/uhh2.AnalysisModuleRunner.*
+    rm -r /nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/BoostedSingleTop/config/workdir_analysis_muo
+elif [ "$answer" == "e" ]
     then
-    #rm -r /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Electron/workdir_analysis_ele/
-    rm -r /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/PreSelection/Muon/workdir_PreSel_muo/
-    #rm /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Electron/uhh2.AnalysisModuleRunner.*
-    #rm /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Muon/uhh2.AnalysisModuleRunner.*
-    #rm -r /nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/BoostedSingleTop/config/workdir_analysis*
+    rm -r /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Electron/workdir_analysis_ele/
+    rm /nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Electron/uhh2.AnalysisModuleRunner.*
+    rm -r /nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/BoostedSingleTop/config/workdir_analysis_ele
 else
     echo "No valid answer. Did nothing."
     exit 1
