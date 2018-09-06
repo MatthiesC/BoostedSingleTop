d4d5b9cc67d2bd57299a7ae1bcb606b8172ff0dd
Christopher Matthies <Christopher.Matthies@desy.de>
2018-09-06 17:43:59 +0200

    include e+jets into writeInputHists

--- theta-workdir/writeInputHists.cxx
+++ theta-workdir/writeInputHists.cxx
@@ -1,13 +1,11 @@
 #include <vector>
 #include <iostream>
 
-void copyAndRenameHists(TString pt_bin)
+void copyAndRenameHists(TString pt_bin, TString channel)
 {
-  std::cout << "WARNING:  Do not forget to run SFramePlotter first!  Else, there are no input root files.  Do not forget to merge single-top s+t channels." << std::endl;
-
-  TFile* final_file = TFile::Open("theta-input_PT-"+pt_bin+".root", "RECREATE"); // my output file
+  TFile* final_file = TFile::Open("input/"+channel+"/theta-input_PT-"+pt_bin+".root", "RECREATE"); // my output file
   
-  TString pathName = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/Muon/";
+  TString pathName = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/"+channel+"/";
   TString fileName_prefix = "uhh2.AnalysisModuleRunner.";
   std::vector<TString> fileNames_DATA {"DATA"};
   std::vector<TString> fileNames_MC {"SingleTop_tWch", "SingleTop_stch", "TTbar", "WJets", "DYJets", "Diboson", "QCD"};
@@ -20,7 +18,7 @@ void copyAndRenameHists(TString pt_bin)
 	  TString input_file = pathName+fileName_prefix+(i==0 ? "DATA." : "MC.")+(fileNames.at(i)).at(j)+".root";
 	  std::cout << "Copying hists from input file:   " << input_file << std::endl;
 	  TFile *input_file_ = TFile::Open(input_file, "read");
-	  TH1F *input_histo = (TH1F*)input_file_->Get("MVA/"+pt_bin+"_mvaD_rebin01");
+	  TH1F *input_histo = (TH1F*)input_file_->Get("MVA_1t/"+pt_bin+"_mvaD_rebin01");
 	  input_histo->SetName(pt_bin+"__"+fileNames.at(i).at(j)+"__for-theta");
 	  final_file->cd(); // change to this file to be able to write on it
 	  input_histo->Write();
@@ -33,8 +31,10 @@ void copyAndRenameHists(TString pt_bin)
 void writeInputHists()
 {
   std::vector<TString> pt_bins {"200to300", "300to400", "400to600", "600to1200"};
+  std::cout << "WARNING:  Do not forget to run SFramePlotter first!  Else, there are no input root files.  Do not forget to merge single-top s+t channels." << std::endl;
   for(int bin = 0; bin < pt_bins.size(); ++bin)
     {
-      copyAndRenameHists(pt_bins.at(bin));
+      copyAndRenameHists(pt_bins.at(bin),"Muon");
+      copyAndRenameHists(pt_bins.at(bin),"Electron");
     }
 }
