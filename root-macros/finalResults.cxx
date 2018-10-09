671fa0260b629fc6ea65f7f0151f7b982b966db5
Christopher Matthies <Christopher.Matthies@desy.de>
2018-10-10 00:41:28 +0200

    update macros

--- root-macros/finalResults.cxx
+++ root-macros/finalResults.cxx
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <vector>
 
-vector<double> recoEfficiency(TString channel) {
+vector<double> recoEfficiency(TString channel, TString n_btags) {
    
   TString input_path_rec = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Analysis/"+channel+"/";
   TString input_path_gen = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Generator/SingleTop_tWch/";
-  cout << "Make sure to already have run SFramePlotter over the input files to have the hadded tW sample (top+antitop)!" << endl;
+  std::cout << "Make sure to already have run SFramePlotter over the input files to have the hadded tW sample (top+antitop)!" << std::endl;
   TFile* input_file_rec = TFile::Open(input_path_rec+"uhh2.AnalysisModuleRunner.MC.SingleTop_tWch.root", "READ");
   TFile* input_file_gen = TFile::Open(input_path_gen+"uhh2.AnalysisModuleRunner.MC.SingleTop_tWch.root", "READ");
-  TH1F* hist_rec = (TH1F*)input_file_rec->Get("Matching_tW/topjet_pt_rebin");
+  TString input_hist_rec = "MVA_1t"+n_btags+"/nocuts_top_pt_rebin";
+  TH1F* hist_rec = (TH1F*)input_file_rec->Get(input_hist_rec);
   TString genhist;
   if(channel == "Electron") genhist = "nocuts/EleChannel_TopPt_GenCut";
   else if(channel == "Muon") genhist = "nocuts/MuoChannel_TopPt_GenCut";
-  TH1F* hist_gen = (TH1F*)input_file_gen->Get(genhist);
+  TH1F* hist_gen_raw = (TH1F*)input_file_gen->Get(genhist);
+
+  //double hf = 1./0.75;
 
   TCanvas* c = new TCanvas("c","",600,800);
 
@@ -28,6 +31,11 @@ vector<double> recoEfficiency(TString channel) {
   upper->Draw();
   upper->cd();
   hist_rec->Draw("hist e1");
+  TH1F* hist_gen = (TH1F*)hist_rec->Clone();
+  for(int i = 1; i < 5; i++) {
+    hist_gen->SetBinContent(i,hist_gen_raw->GetBinContent(i+1));
+    hist_gen->SetBinError(i,hist_gen_raw->GetBinError(i+1));
+    }
   hist_gen->Draw("hist e1 same");
 
   gStyle->SetOptStat(kFALSE);
@@ -36,7 +44,7 @@ vector<double> recoEfficiency(TString channel) {
   hist_rec->SetLineWidth(2);
   hist_rec->SetLineColor(597); // nice blue
   hist_rec->SetLineStyle(1);
-  hist_rec->SetMinimum(0.4); // avoid showing the zero at y=0
+  hist_rec->SetMinimum(4.0); // avoid showing the zero at y=0
 
   hist_gen->SetTitle("");
   hist_gen->SetLineWidth(2);
@@ -49,7 +57,8 @@ vector<double> recoEfficiency(TString channel) {
   hist_rec->GetYaxis()->SetLabelSize(0.05);
   hist_rec->GetXaxis()->SetTitle("");
   hist_rec->GetXaxis()->SetTitleOffset(1.4);
-  hist_rec->SetMaximum(10.*hist_gen->GetBinContent(2));
+  hist_rec->GetXaxis()->SetNdivisions(507);
+  hist_rec->SetMaximum(10.*hist_gen->GetBinContent(1));
   upper->SetLogy();
 
   //  c->cd();
@@ -61,6 +70,17 @@ vector<double> recoEfficiency(TString channel) {
   simulation->SetTextSize(24);
   simulation->Draw();
 
+  TString category_1 = "tW #rightarrow ";
+  TString category_2 = (channel == "Electron" ? "e+jets " : "#mu+jets ");
+  TString category_3 = (n_btags == "1b" ? "(1 b-tag)" : "(#geq 2 b-tags)");
+
+  TLatex *category = new TLatex(.2,1-0.09*6/5-0.1,category_1+category_2+category_3);
+  category->SetNDC();
+  //category->SetTextAlign(12);
+  category->SetTextFont(43);
+  category->SetTextSize(24);
+  category->Draw();
+
   TLatex *lumi = new TLatex(.95,1-0.09*6/5,"35.9 fb^{-1} (13 TeV)");
   lumi->SetNDC();
   lumi->SetTextAlign(31);
@@ -71,8 +91,11 @@ vector<double> recoEfficiency(TString channel) {
   auto legend = new TLegend(0.65,0.52,0.92,0.80);
   legend->AddEntry(hist_rec, "reco. level");
   legend->AddEntry(hist_gen, "gen. level");
-  if(channel == "Electron") legend->SetHeader("tW #rightarrow e+jets");
-  else if(channel == "Muon") legend->SetHeader("tW #rightarrow #mu+jets");
+  legend->SetHeader("Selection type:");
+  /*if(channel == "Electron" && n_btags == "1b") legend->SetHeader("tW #rightarrow e+jets (1 b-tag)");
+  else if(channel == "Muon" && n_btags == "1b") legend->SetHeader("tW #rightarrow #mu+jets (1-btag)");
+  else if(channel == "Electron" && n_btags == "2b") legend->SetHeader("tW #rightarrow e+jets (#geq 2 b-tags)");
+  else if(channel == "Muon" && n_btags == "2b") legend->SetHeader("tW #rightarrow #mu+jets (#geq 2 b-tags)");*/
   legend->Draw();
   legend->SetBorderSize(0);
 
@@ -93,20 +116,30 @@ vector<double> recoEfficiency(TString channel) {
 
   hist_eff->SetBinContent(1,0);
   double uncertainty, bincontent;
-  for(int i = 2; i < hist_eff->GetNbinsX()+1; i++) {
+  for(int i = 1; i < hist_eff->GetNbinsX()+1; i++) {
     bincontent = hist_rec->GetBinContent(i)/hist_gen->GetBinContent(i);
     hist_eff->SetBinContent(i,bincontent);
     uncertainty = bincontent*sqrt(pow(hist_rec->GetBinError(i)/hist_rec->GetBinContent(i),2)+pow(hist_gen->GetBinError(i)/hist_gen->GetBinContent(i),2));
     hist_eff->SetBinError(i,uncertainty);
     result.push_back(bincontent);
   }
-  //hist_eff->SetMinimum(1.15);
-  //hist_eff->SetMaximum(2.05);
-  hist_eff->SetMaximum(0.14);
-  hist_eff->GetYaxis()->SetNdivisions(503);
+  hist_eff->SetMaximum(1.3*hist_eff->GetMaximum());
+  hist_eff->SetMinimum(0.3*hist_eff->GetMinimum());
+  if(n_btags == "1b") {
+    hist_eff->SetMaximum(0.15);
+    hist_eff->SetMinimum(0.05);
+    hist_eff->GetYaxis()->SetNdivisions(405);
+  }
+  else if(n_btags == "2b") {
+    hist_eff->SetMaximum(0.075);
+    hist_eff->SetMinimum(0.005);
+    hist_eff->GetYaxis()->SetNdivisions(404);
+  }
+  //hist_eff->GetYaxis()->SetNdivisions(503);
   hist_eff->GetYaxis()->SetLabelSize(0.05/(1-splitter));
   hist_eff->GetXaxis()->SetLabelSize(0.05/(1-splitter));
   hist_eff->GetXaxis()->SetTitleOffset(1.6);
+  hist_eff->GetXaxis()->SetNdivisions(507);
   hist_eff->SetLineStyle(1);
 
   hist_eff->SetLineColor(kBlack);
@@ -118,36 +151,105 @@ vector<double> recoEfficiency(TString channel) {
   hist_eff->GetYaxis()->SetTitleOffset(0.9);
 
   hist_eff->Draw("e1");
+
+  TGaxis *axis = new TGaxis(200,hist_eff->GetMaximum(),1200,hist_eff->GetMaximum(),200,1200,507,"-");
+  axis->Draw();
  
   //==================//
   // SAVE AS EPS PLOT //
   //==================//
 
   c->cd();
-  c->SaveAs("recoEfficiency_"+channel+".eps");
+  c->SaveAs("recoEfficiency_"+channel+"_"+n_btags+".eps");
 
   return result;
 }
 
 
-void eventCountPlots(TString channel, vector<double> efficiencies) {
+vector<double> getDataPoints(TString channel, TString n_btags) {
 
-  TString input_path_measurement = "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/BoostedSingleTop/theta-workdir/rootfiles/theta-output_stat.root";
+  TString input_path_measurement = "/nfs/dust/cms/user/matthies/Analysis_80x_v5/CMSSW_8_0_24_patch1/src/UHH2/BoostedSingleTop/theta-workdir/rootfiles/theta-input_stat.root";
   cout << "Make sure to run THETA to get your input histograms!" << endl;
   TFile* input_file_measurement = TFile::Open(input_path_measurement, "READ");
-  TString input_hist_measurement = "";
-  TH1F* hist_measured = (TH1F*)input_file_measurement->Get(input_hist_measurement);
+
+  vector<double> result;
+
+  for(TString topptbin : {"200to300", "300to400", "400to600", "600to1200"}) {
+
+    TString input_hist_measurement = "BDT_"+topptbin+"_"+n_btags+(channel == "Electron" ? "_ele__" : "_muo__")+"SingleTop_tWch";
+    TH1F* hist_measured = (TH1F*)input_file_measurement->Get(input_hist_measurement);
+
+    hist_measured->Rebin(hist_measured->GetSize()-2);
+    result.push_back(hist_measured->GetBinContent(1));
+    result.push_back(hist_measured->GetBinError(1));
+  }
+
+  return result;
+}
+
+
+void makeFinalPlots(TString channel, TString n_btags, vector<double> datapoints) {
+
+  TString input_path_gen = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Generator/SingleTop_tWch/";
+  std::cout << "Make sure to already have run SFramePlotter over the input files to have the hadded tW sample (top+antitop)!" << std::endl;
+  TFile* input_file_gen = TFile::Open(input_path_gen+"uhh2.AnalysisModuleRunner.MC.SingleTop_tWch.root", "READ");
+  TString genhist;
+  if(channel == "Electron") genhist = "nocuts/EleChannel_TopPt_GenCut";
+  else if(channel == "Muon") genhist = "nocuts/MuoChannel_TopPt_GenCut";
+  TH1F* hist_gen = (TH1F*)input_file_gen->Get(genhist);
+
+  TCanvas* c = new TCanvas("c","",600,600);
+
+  hist_gen->Draw("hist");
+  hist_gen->SetTitle("");
+  hist_gen->GetXaxis()->SetTitle("top-quark p_{T} [GeV]");
+  hist_gen->GetYaxis()->SetTitle("d#sigma/dp_{T} [pb]");
+  hist_gen->SetLineColor(597);
+  hist_gen->SetLineStyle(2);
+  hist_gen->SetLineWidth(2);
+  //hist_gen->GetXaxis()->SetRange(3,5); // used to show less top-pt bins, useful to increase visibility of higher-pt bins
+
+  TH1F* hist_data = (TH1F*)hist_gen->Clone();
+  for(int i = 2; i < 6; i++) {
+    hist_data->SetBinContent(i,datapoints.at((i-2)*2));
+    hist_data->SetBinError(i,datapoints.at((i-2)*2+1));
+  }
+  
+  hist_data->Draw("p same e1");
+  hist_data->SetLineColor(kBlack);
+  hist_data->SetLineStyle(1);
+  hist_data->SetMarkerStyle(8);
+
+  double lumi = 1/35867.;
+  hist_gen->Scale(lumi);
+  hist_data->Scale(lumi);
+
+  //c->SetLogy();
+
+  //==================//
+  // SAVE AS EPS PLOT //
+  //==================//
+
+  c->cd();
+  c->SaveAs("diffXsection_"+channel+"_"+n_btags+".eps");
+
 }
 
 
 void finalResults() {
 
   for(TString channel : {"Electron", "Muon"}) {
-    auto efficiencies = recoEfficiency(channel);
-    cout << "Results for the " << channel << " channel:" << endl;
-    for(int i = 0; i < efficiencies.size(); i++) {
-      cout << efficiencies.at(i) << endl;
+    for(TString n_btags : {"1b", "2b"}) {
+      auto efficiencies = recoEfficiency(channel, n_btags);
+      cout << "Results for the " << channel << " channel (" << n_btags << " category):" << endl;
+      for(int i = 0; i < efficiencies.size(); i++) {
+	cout << efficiencies.at(i) << endl;
+      }
+      auto datapoints = getDataPoints(channel, n_btags);
+      for(int i = 0; i < datapoints.size(); i++) {
+	datapoints.at(i) = datapoints.at(i)/efficiencies.at(i/2);
+      }
+      makeFinalPlots(channel, n_btags, datapoints);
     }
-    eventCountPlots(channel, efficiencies);
   }
 }
--- root-macros/pttopspectrum.cxx
+++ root-macros/pttopspectrum.cxx
@@ -3,6 +3,8 @@
 
 void pttopspectrum() {
    
+  bool bNormalized = false;
+
   TString input_path = "/nfs/dust/cms/user/matthies/BoostedSingleTop/RunII_80X_v5/Generator/SingleTop/";
   std::cout << "Make sure to already have run SFramePlotter over the input files to have the hadded ones." << std::endl;
   std::vector<TFile*> input_files;
@@ -11,7 +13,11 @@ void pttopspectrum() {
     TFile* input_file = TFile::Open(input_path+"uhh2.AnalysisModuleRunner.MC.SingleTop_"+channel+".root", "READ");
     input_files.push_back(input_file);
     TH1F* hist = (TH1F*)input_file->Get("singletopgen/Pt_top");
-    TH1F* hist2 = (TH1F*)input_file->Get("singletopgen/Pt_top");
+    TH1F* hist2 = (TH1F*)hist->Clone("hist2");
+    //TF1 *f1 = new TF1("f1","1.2",0,2000);
+    //hist2->Multiply(f1,1);
+    //Double_t shift = 0.5*hist->GetBinWidth(1);
+    //hist2->GetXaxis()->SetLimits(0+shift,2000+shift);
     hists.push_back(hist);
     hists2.push_back(hist2);
   }
@@ -24,35 +30,42 @@ void pttopspectrum() {
   THStack *hs = new THStack("hs","");
   THStack *hs2 = new THStack("hs2","");
 
-  for(Int_t i = 0; i < 3; i++) {
-    hists.at(i)->SetLineWidth(3);
-    hists2.at(i)->SetLineWidth(3);
+  for(Int_t i = 2; i > -1; i--) {
+    hists.at(i)->SetLineWidth(2);
+    //hists2.at(i)->SetLineWidth(3);
     hists.at(i)->Rebin(50);
-    hists2.at(i)->Rebin(50);
+    if(bNormalized){
+      Double_t scale = 1./(hists.at(i)->Integral());
+      std::cout << scale << std::endl;
+      hists.at(i)->Scale(scale);
+    }
+    //hists2.at(i)->Rebin(50);
     hs->Add(hists.at(i));
-    hs2->Add(hists2.at(i));
+    //hs2->Add(hists2.at(i));
   }
 
-  //hists.at(0)->SetFillColor(597);
   hists.at(0)->SetLineColor(597);
-  //hists.at(1)->SetFillColor(kRed+1);
-  hists.at(1)->SetLineColor(kRed+1);
-  //hists.at(2)->SetFillColor(kOrange+7);
-  hists.at(2)->SetLineColor(kOrange+7);
+  //hists.at(0)->SetLineStyle(1);
+  hists.at(1)->SetLineColor(kOrange+7);
+  hists.at(1)->SetLineStyle(2);
+  hists.at(2)->SetLineColor(kGreen+2);
+  hists.at(2)->SetLineStyle(5);
 
   //hists.at(0)->SetFillStyle(3354);
   //hists.at(1)->SetFillStyle(3345);
   //hists.at(2)->SetFillStyle(3354);
 
-  hs->Draw("nostack hist");
+  hs->Draw("nostack hist e1");
   //hs2->Draw("nostack hist same");
 
   //gStyle->SetPalette(kOcean);
-  hs->GetYaxis()->SetTitle("Events");
+  hs->GetYaxis()->SetTitle((!bNormalized ? "Events" : "#DeltaN/N"));
   hs->GetYaxis()->SetTitleOffset(1.4);
-  hs->GetXaxis()->SetTitle("top-quark p_{T}");
+  hs->GetXaxis()->SetTitle("top-quark p_{T} [GeV]");
   hs->GetXaxis()->SetTitleOffset(1.4);
   hs->SetMinimum(0.4);
+  if(bNormalized) hs->SetMinimum(0.4e-7);
+
 
   c->cd();
 
@@ -63,12 +76,21 @@ void pttopspectrum() {
   simulation->SetTextSize(24);
   simulation->Draw();
 
-  auto legend = new TLegend(0.7,0.7,0.9,0.9);
+  TLatex *lumi = new TLatex(.9,.91,"35.9 fb^{-1} (13 TeV)");
+  lumi->SetNDC();
+  lumi->SetTextAlign(31);
+  lumi->SetTextFont(43);
+  lumi->SetTextSize(24);
+  lumi->Draw();
+
+  auto legend = new TLegend(0.65,0.65,0.85,0.85);
   legend->AddEntry(hists.at(0), "tW");
   legend->AddEntry(hists.at(1), "t-ch.");
   legend->AddEntry(hists.at(2), "s-ch.");
   legend->Draw();
+  legend->SetBorderSize(0);
 
-  c->SaveAs("pttopspectrum.eps");
+  if(bNormalized) c->SaveAs("pttopspectrum_norm.eps");
+  else c->SaveAs("pttopspectrum.eps");
 
 }
